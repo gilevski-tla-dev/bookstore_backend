e19d60ad00c8ed9f0c3284562d763fcc83edc156
Nikita Gilevski <gilevskini2004@gmail.com>
2024-06-08 03:57:16 +0200

    prepare_routing/endpoints
    Endpoints and routings(users/books) were prepared and the architecture of the application was slightly modified.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from 'src/user/user.module';
+import { BookModule } from 'src/book/book.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/book/book.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BookService {
+  getBooks() {
+    return 'books';
+  }
+}
